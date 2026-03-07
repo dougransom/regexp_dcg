21de3a9804d5a50d7cea72374be284c56ddd0bef
Doug Ransom <doug@ransom.vip>
2026-03-07 09:40:25 -0800

    checkpoint

--- regexp_ast.pl
+++ regexp_ast.pl
@@ -1,193 +1,99 @@
-:- dynamic(metachars/1).
-:- module(regexp_ast, [re_expr//1,re_dot//0, re_caret//0, re_dollar//0, 
-        re_star//0, re_plus//0, re_question//0, re_backslash//0, 
-        re_pipe//0, re_left_paren//0, re_right_paren//0, 
-        re_left_brace//0, re_right_brace//0, re_left_bracket//0, 
-        re_char//1,
-        re_right_bracket//0, re_metachar//0,metachars/1,  
-        re_chars//1,re_suffix//1]).
+:- module(regexp_ast, [
+    re_expr//1
+]).
 
-:-use_module(library(dcgs)).
-:-use_module(library(reif)).
+:- use_module(library(dcgs)).
+:- use_module(library(lists)).
 
-%https://docs.python.org/3/library/re.html#re-syntax
-
-
-%all metacharacters, in sorted order
-
-
-metachars_strings(Ss) :- findall(L,phrase(re_metachar,L),Bs),sort(Bs,Ss).
-
-%todo memoize this
-metachars(Cs) :-
-    metachars_strings(Lists),
-    phrase(flatten_lists(Lists), Cs).
-
-flatten_lists([]) --> [].
-flatten_lists([[C]|Rest]) --> [C], flatten_lists(Rest).
-
- 
-metachar_t(C, T) :-
-    metachars(MCs),
-    memberd_t(C, MCs, T).
-
-postfix_chars("*+?").
-
-re_dot --> ".". 
-re_caret --> "^".
-re_dollar --> "$".
-re_star --> "*".
-re_plus --> "+".
-re_question --> "?".
-re_backslash --> "\\".
-re_pipe --> "|".
-re_left_paren --> "(".
-re_right_paren --> ")". 
-re_left_brace --> "{".
-re_right_brace --> "}".
-re_left_bracket --> "[".
-re_right_bracket --> "]".
-
-re_metachar --> re_dot
-    | re_caret
-    | re_dollar
-    | re_star
-    | re_plus
-    | re_question
-    | re_backslash
-    | re_pipe
-    | re_left_paren
-    | re_right_paren
-    | re_left_brace
-    | re_right_brace
-    | re_left_bracket
-    | re_right_bracket.
-
-postfix_char_t(C, T) :-
-    postfix_chars(Ps),
-    memberd_t(C, Ps, T).
-
-
-
-% re_expr(re_dollar) --> re_dollar.
-% re_expr(re_dot) --> re_dot.
-
-% re_expr(re_chars([C|Cs])) -->
-%     [C],
-%     { metachar_t(C, false) },
-%     re_chars(Cs).
-
-% re_expr(re_postfix(Sub, Postfix)) --> re_postfix(Sub, Postfix).
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% 1. TOKENIZER — flat list of atoms
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 re_expr(AST) -->
-    re_term(T),
-    re_expr_rest(T, AST).
+    flat_expr(Tokens),
+    { bind_postfix(Tokens, P1),
+      parse_concat(P1, AST0),
+      parse_alternation(AST0, AST)
+    }.
 
+%% flat_expr(-Atoms)
+flat_expr([A|As]) -->
+    flat_atom(A),
+    flat_expr(As).
+flat_expr([]) --> [].
 
-re_expr_rest(Acc, AST) -->
-    re_term(T),
-    { Acc2 = re_concat(Acc, T) },
-    re_expr_rest(Acc2, AST).
+%% flat_atom(-Atom)
+flat_atom(lit(Cs)) -->
+    literal_run(Cs).
 
-re_expr_rest(AST, AST) --> [].
+flat_atom(dot)       --> ".".
+flat_atom(caret)     --> "^".
+flat_atom(dollar)    --> "$".
+flat_atom(lparen)    --> "(".
+flat_atom(rparen)    --> ")".
+flat_atom(lbrack)    --> "[".
+flat_atom(rbrack)    --> "]".
+flat_atom(pipe)      --> "|".
+flat_atom(star)      --> "*".
+flat_atom(plus)      --> "+".
+flat_atom(question)  --> "?".
 
-re_term(AST) -->
-    re_atom(A),
-    re_term_suffix(A, AST).
+flat_atom(escaped(C)) -->
+    "\\", [C].
 
-re_term_suffix(A, AST) -->
-    re_suffix(S),
-    { adjust_postfix(A, S, AST) }.
-
-re_term_suffix(A, A) --> [].
-
-
-adjust_postfix(re_chars(Cs), S, AST) :-
-    split_last(Cs, Init, Last),
-    (   Init = []
-    ->  AST = re_postfix(re_chars([Last]), S)
-    ;   AST = re_concat(
-              re_chars(Init),
-              re_postfix(re_chars([Last]), S)
-          )
-    ).
-
-adjust_postfix(Other, S, re_postfix(Other, S)).
-
-
-split_last([X], [], X).
-split_last([H|T], [H|Init], Last) :-
-    split_last(T, Init, Last).
-
-re_atom(re_chars(Cs)) --> re_chars(Cs).
-re_atom(re_dot)       --> re_dot.
-re_atom(re_dollar)    --> re_dollar.
-
-
-%TODO this is causing infinfite loop when a postfix expression like .* 
-%re_expr(re_concat(L,R)) --> re_expr(L), re_expr(R).
-
-
-
-
-
-
-% re_postfix(re_expr(Sub),re_suffix(Postfix)) --> re_expr(Sub), re_suffix(Postfix).
-
-
-
-% Postifix Operators
-
-
-
-%toast it re_postfix_expr(Re_Left,Postfix ) -->  re_not_empty(Re_Left), re_postfix(Postfix).
-
-
-re_suffix(re_star) --> re_star.
-re_suffix(re_question) --> re_question.
-re_suffix(re_plus) --> re_plus.
- 
-re_chars([C|Cs]) -->
+%% literal_run(-Chars)
+literal_run([C|Cs]) -->
     [C],
-    { metachar_t(C, false) },
-    re_chars_more(Cs).
+    { \+ metachar(C) },
+    literal_run_more(Cs).
 
-re_chars_more([C|Cs]) -->
+literal_run_more([C|Cs]) -->
     [C],
-    { metachar_t(C, false) },
-    re_chars_more(Cs).
+    { \+ metachar(C) },
+    literal_run_more(Cs).
+literal_run_more([]) --> [].
 
-re_chars_more([]) --> [].
+%% metachar(+Char)
+metachar(C) :-
+    member(C, `.^$*+?()[]|\\{}`).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% 2. POSTFIX BINDING
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+bind_postfix([], []).
 
-peek_code(C, [C|S], [C|S]).
+bind_postfix([lit(Cs), plus | Rest], [postfix(lit(Cs), plus) | Out]) :-
+    bind_postfix(Rest, Out).
 
- 
+bind_postfix([lit(Cs), star | Rest], [postfix(lit(Cs), star) | Out]) :-
+    bind_postfix(Rest, Out).
 
-re_char(C) -->
-    [C],
-    { metachar_t(C, false) }.   % normal literal
+bind_postfix([lit(Cs), question | Rest], [postfix(lit(Cs), question) | Out]) :-
+    bind_postfix(Rest, Out).
 
-re_char(C) -->
-    "\\",                       % escaped metachar
-    [C],
-    { metachar_t(C, true) }.
+bind_postfix([A | Rest], [A | Out]) :-
+    bind_postfix(Rest, Out).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% 3. CONCATENATION (left fold)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% re_char(C) --> [C], {metachars(MCs),  memberd_t(C,MCs,false)}.
-% re_char(C) --> re_backslash, [C], {metachars(MCs), memberd_t(C,MCs,true)}.
+parse_concat([A], A).
+parse_concat([A,B|Rest], concat(A, C)) :-
+    parse_concat([B|Rest], C).
 
-%represent a sequence of chars as a list
-% re_chars([C|Cs]) --> re_char(C), re_chars(Cs). 
-% re_chars([]) --> [].
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% 4. ALTERNATION
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%relate re_chars to a DCG
+parse_alternation(concat(A, concat(pipe, B)), or(A1, B1)) :-
+    parse_alternation(A, A1),
+    parse_alternation(B, B1).
 
-ast_dcg(re_chars(Chars), Chars  ).
+parse_alternation(concat(A, B), concat(A1, B1)) :-
+    parse_alternation(A, A1),
+    parse_alternation(B, B1).
 
-
-        
-
-
+parse_alternation(pipe, pipe).  % shouldn't appear alone
+parse_alternation(X, X).
