a263af74022b00700fad574760199a19d590c1dd
Doug Ransom <doug@ransom.vip>
2026-03-15 09:13:35 -0700

    .

---
+++ test_regexp_compile_dcg.pl
@@ -0,0 +1,7 @@
+:- use_module(bakage).
+:- use_module(pkg(testing)).
+:- use_module(regexp_ast).
+:- use_module(regexp_compile_dcg).
+:- use_module(library(debug)).
+:- use_module(library(pio)).
+:- use_module(library(dcgs)).
