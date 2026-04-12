7bd6ba017fae9109d6a3ede58e2e05fbbf051930
Doug Ransom <doug@ransom.vip>
2026-04-12 10:27:01 -0700

    .

--- ast_dcg.pl
+++ ast_dcg.pl
@@ -4,11 +4,14 @@
 
 dcg_combinator(dcg_longest_or).
 dcg_combinator(dcg_capture).
-term_expansion(DCG0, DCG) :-
-    DCG0 =.. [Functor | Args],
+
+term_expansion(T0, ast_dcg:T0) :-
+    T0 =.. [Functor | _],
     dcg_combinator(Functor),
-    !,
-    DCG =.. [Functor, ast_dcg | Args].
+    \+ T0 = (_:_),   % do NOT rewrite already-qualified terms
+    !.
+
+
 %simple literal match, just use the literal as a DCG.
 ast_dcg(lit(Lit),Lit).
 
@@ -16,24 +19,24 @@ ast_dcg(lit(Lit),Lit).
 ast_dcg(or(L, R), DCG) :-
     ast_dcg(L, LDCG),
     ast_dcg(R, RDCG),
-    DCG = ast_dcg:dcg_longest_or(LDCG, RDCG, _Match).
+    DCG = dcg_longest_or(LDCG, RDCG, _Match).
 
 
 %capture construction.  
 
-% ast_dcg(capture(Inner), dcg_capture(InnerDCG,Captured)) :-   
-%     ast_dcg(Inner, InnerDCG).    
+ast_dcg(capture(Inner), dcg_capture(InnerDCG,Captured)) :-   
+     ast_dcg(Inner, InnerDCG).    
       
-% dcg_capture(Inner, Match, S0, S) :-
-%     phrase(Inner, S0, S),
-%     append(Match, S, S0).
+dcg_capture(Inner, Match, S0, S) :-
+     phrase(Inner, S0, S),
+     append(Match, S, S0).
 
-% dcg_capture(Inner, Match) -->
-%     dcg_capture(Inner, Match).
+ dcg_capture(Inner, Match) -->
+     dcg_capture(Inner, Match).
 
-dcg_longest_or(Module,L, R, Match, S0, S) :-
-    phrase(Module:dcg_capture(L, ML), S0, SL),
-    phrase(Module:dcg_capture(R, MR), S0, SR),
+dcg_longest_or(L, R, Match, S0, S) :-
+    phrase(dcg_capture(L, ML), S0, SL),
+    phrase(dcg_capture(R, MR), S0, SR),
     length(ML, LenL),
     length(MR, LenR),
     (   LenL >= LenR
@@ -41,6 +44,6 @@ dcg_longest_or(Module,L, R, Match, S0, S) :-
     ;   Match = MR, S = SR
     ).
 
-dcg_longest_or(Module,L, R, Match) -->
-    Module:dcg_longest_or(Module,L, R, Match).
+dcg_longest_or(L, R, Match) -->
+    dcg_longest_or(L, R, Match).
 
